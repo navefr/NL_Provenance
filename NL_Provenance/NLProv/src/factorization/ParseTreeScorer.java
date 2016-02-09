b5164331e46622e4713a7730eaeb56c0063e9996
nfrost <nfrost@paypal.com>
2016-02-09 16:33:09 +0200

    ParseTreScorer create Basic Trees

--- NL_Provenance/NLProv/src/factorization/ParseTreeScorer.java
+++ NL_Provenance/NLProv/src/factorization/ParseTreeScorer.java
@@ -2,7 +2,12 @@ package factorization;
 
 import dataStructure.ParseTree;
 import dataStructure.ParseTreeNode;
-import treedist.*;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import treedist.BasicTree;
+import treedist.ComparisonZhangShasha;
+import treedist.OpsZhangShasha;
+import treedist.Transformation;
 
 import java.util.*;
 
@@ -12,7 +17,8 @@ import java.util.*;
 public class ParseTreeScorer {
     private ParseTree queryTree;
     private ParseTree answerTree;
-    Map<ParseTreeNode, Collection<ParseTreeNode>> nodeMappings;
+    private Map<ParseTreeNode, Collection<ParseTreeNode>> nodeMappings;
+    private Map<ParseTreeNode, ParseTreeNode> nodeMappingsReverse;
 
     public ParseTreeScorer(ParseTree queryTree, ParseTree answerTree, Map<ParseTreeNode, Collection<ParseTreeNode>> nodeMappings) {
         copyTrees(queryTree, answerTree, nodeMappings);
@@ -23,26 +29,64 @@ public class ParseTreeScorer {
         ComparisonZhangShasha treeCorrector = new ComparisonZhangShasha();
         OpsZhangShasha costs = new OpsZhangShasha();
 
-        Transformation transform = treeCorrector.findDistance(convertTreeDS(queryTree), convertTreeDS(answerTree), costs);
+        Pair<BasicTree, BasicTree> basicTrees = getBasicTrees();
+        Transformation transform = treeCorrector.findDistance(basicTrees.getLeft(), basicTrees.getRight(), costs);
 
         return -transform.getCost();
     }
 
-    private static BasicTree convertTreeDS(ParseTree parseTree) {
-        Hashtable<String, ArrayList<String>> tree = new Hashtable<String, ArrayList<String>>();
-        for (ParseTreeNode node : parseTree.allNodes) {
+
+    private Pair<BasicTree, BasicTree> getBasicTrees() {
+        Map<ParseTreeNode, String> queryNodeToIdentifier = new HashMap<ParseTreeNode, String>();
+        Map<ParseTreeNode, String> answerNodeToIdentifier = new HashMap<ParseTreeNode, String>();
+
+        Hashtable<String, ArrayList<String>> queryTreeTable = new Hashtable<String, ArrayList<String>>();
+        for (ParseTreeNode node : queryTree.allNodes) {
             ArrayList<String> nodeChildren = new ArrayList<String>();
             for (ParseTreeNode child : node.children) {
-                nodeChildren.add(getNodeIdentifier(child));
+                nodeChildren.add(getQueryNodeIdentifier(child, queryNodeToIdentifier));
             }
-            tree.put(getNodeIdentifier(node), nodeChildren);
+            queryTreeTable.put(getQueryNodeIdentifier(node, queryNodeToIdentifier), nodeChildren);
         }
-        return new BasicTree(tree, getNodeIdentifier(parseTree.root), BasicTree.POSTORDER);
+        BasicTree queryBasicTree = new BasicTree(queryTreeTable, getQueryNodeIdentifier(queryTree.root, queryNodeToIdentifier), BasicTree.POSTORDER);
+
+        Hashtable<String, ArrayList<String>> answerTreeTable = new Hashtable<String, ArrayList<String>>();
+        for (ParseTreeNode node : answerTree.allNodes) {
+            ArrayList<String> nodeChildren = new ArrayList<String>();
+            for (ParseTreeNode child : node.children) {
+                nodeChildren.add(getAnswerNodeIdentifier(child, queryNodeToIdentifier, answerNodeToIdentifier));
+            }
+            answerTreeTable.put(getAnswerNodeIdentifier(node, queryNodeToIdentifier, answerNodeToIdentifier), nodeChildren);
+        }
+        BasicTree answerBasicTree = new BasicTree(answerTreeTable, getAnswerNodeIdentifier(answerTree.root, queryNodeToIdentifier, answerNodeToIdentifier), BasicTree.POSTORDER);
+
+        return new ImmutablePair<BasicTree, BasicTree>(queryBasicTree, answerBasicTree);
     }
 
-    // TODO nave - won't work - since query and ans has diff wordOrder
-    private static String getNodeIdentifier(ParseTreeNode node) {
-        return node.label + node.wordOrder;
+    private String getQueryNodeIdentifier(ParseTreeNode node, Map<ParseTreeNode, String> queryNodeToIdentifier) {
+        if (queryNodeToIdentifier.containsKey(node)){
+            return queryNodeToIdentifier.get(node);
+        }
+        String identifier = node.label + node.wordOrder;
+        queryNodeToIdentifier.put(node, identifier);
+        return identifier;
+    }
+
+    private String getAnswerNodeIdentifier(ParseTreeNode node, Map<ParseTreeNode, String> queryNodeToIdentifier, Map<ParseTreeNode, String> answerNodeToIdentifier) {
+        if (answerNodeToIdentifier.containsKey(node)){
+            return answerNodeToIdentifier.get(node);
+        }
+
+        String identifier;
+        ParseTreeNode queryNode = this.nodeMappingsReverse.get(node);
+        if (queryNodeToIdentifier.containsKey(queryNode)){
+            // TODO nave - not remove
+            identifier = queryNodeToIdentifier.remove(queryNode);
+        } else {
+            identifier = node.label + node.wordOrder;
+        }
+        answerNodeToIdentifier.put(node, identifier);
+        return identifier;
     }
 
     private void copyTrees(ParseTree queryTree, ParseTree answerTree, Map<ParseTreeNode, Collection<ParseTreeNode>> nodeMappings) {
@@ -67,6 +111,7 @@ public class ParseTreeScorer {
         }
 
         this.nodeMappings = new HashMap<>();
+        this.nodeMappingsReverse = new HashMap<>();
         for (Map.Entry<ParseTreeNode, Collection<ParseTreeNode>> nodeMappingEntry : nodeMappings.entrySet()) {
             ParseTreeNode oldQueryNode = nodeMappingEntry.getKey();
             ParseTreeNode newQueryNode = queryTreeToCopyMapping.get(oldQueryNode);
@@ -76,6 +121,10 @@ public class ParseTreeScorer {
                 newAnswerNodes.add(answerTreeToCopyMapping.get(oldAnswerNode));
             }
             this.nodeMappings.put(newQueryNode, newAnswerNodes);
+            for (ParseTreeNode newAnswerNode : newAnswerNodes) {
+                this.nodeMappingsReverse.put(newAnswerNode, newQueryNode);
+            }
+
         }
     }
 
