e8dbe8667f551682069db1ed50696db4292bc60c
Nave Frost <navefr@hotmail.com>
2016-04-09 23:58:04 +0300

    runtime

---
+++ NL_Provenance/NLProv/src/Experiments2.java
@@ -0,0 +1,168 @@
+import ansgen.MultipleDerivationFactorizedAnswerTreeBuilder;
+import ansgen.MultipleDerivationSummarizedAnswerTreeBuilder;
+import ansgen.SingleDerivationAnswerTreeBuilder;
+import dataStructure.Query;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+import factorization.Expression;
+import factorization.QueryBasedFactorizer;
+import factorization.WordMappings;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.deri.iris.api.basics.IPredicate;
+import org.deri.iris.api.basics.ITuple;
+import org.deri.iris.api.terms.ITerm;
+import org.deri.iris.basics.Tuple;
+import org.deri.iris.storage.IRelation;
+import org.deri.iris.terms.StringTerm;
+import org.w3c.dom.Document;
+import rdbms.RDBMS;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by nfrost on 4/1/2016
+ */
+public class Experiments2 {
+
+    private static Map<IPredicate, IRelation> factMap = null;
+
+    public static void main(String [] args) throws Exception {
+        LexicalizedParser lexiParser = LexicalizedParser.loadModel("edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");
+        RDBMS db = new RDBMS("mas");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document tokens = builder.parse(new File("NL_Provenance/NaLIRWeb/src/zfiles/tokens.xml"));
+
+        String query1 = "return the homepage of SIGMOD. ";
+        String query2 = "return the conferences in database area. ";
+        String query3 = "return the authors who published papers in SIGMOD after 2005. ";
+        String query4 = "return me the authors from \"Tel Aviv University\" who published papers in VLDB. ";
+        String query5 = "return the papers whose title contains \"OASSIS\"";
+        String query6 = "return the authors who published papers in SIGMOD before 2015 and after 2005. ";
+        String query7 = "return the papers which were published in conferences in database area";
+        String query8 = "return the area of conferences";
+        String query9 = "return the authors who published papers in database conferences. ";
+        String query10 = "return the authors who published papers in database conferences after 2005. ";
+        String query11 = "  . ";
+
+        Map<String, String> querySentences = new TreeMap<>();
+        querySentences.put("query00_init", query1);
+//        querySentences.put("query01", query1);
+//        querySentences.put("query02", query2);
+        querySentences.put("query03", query3);
+//        querySentences.put("query04", query4);
+//        querySentences.put("query05", query5);
+//        querySentences.put("query06", query6);
+//        querySentences.put("query07", query7);
+        querySentences.put("query08", query8);
+        querySentences.put("query09", query9);
+//        querySentences.put("query10", query10);
+        querySentences.put("query11", query11);
+
+        for (Map.Entry<String, String> queryEntry : querySentences.entrySet()) {
+
+            String queryName = queryEntry.getKey();
+            String querySentence = queryEntry.getValue();
+
+            System.out.println(queryName);
+
+            Query query = new Query(querySentence, db.schemaGraph);
+
+            components.StanfordNLParser.parse(query, lexiParser);
+            components.NodeMapper.phraseProcess(query, db, tokens);
+            components.EntityResolution.entityResolute(query);
+            components.TreeStructureAdjustor.treeStructureAdjust(query, db);
+            components.Explainer.explain(query);
+            components.SQLTranslator.translate(query, db);
+
+            if (query.blocks.size() == 1) {
+                Map<ITuple, Pair<WordMappings, Integer>> resultsAndWordMappings = getResultsAndWordMappings(queryName);
+
+                System.out.println(String.format("%20s\t%20s\t%20s\t%20s\t%20s\t%20s\t%20s", "#UniqueValues", "#Derivations", "#Elements", "SingleTime", "MultipleTime", "SummarizedTime", "FactorizationSize"));
+                for (Map.Entry<ITuple, Pair<WordMappings, Integer>> resultsAndWordMappingsEntry : resultsAndWordMappings.entrySet()) {
+                    WordMappings wordMappings = resultsAndWordMappingsEntry.getValue().getLeft();
+                    Integer uniqueValues = resultsAndWordMappingsEntry.getValue().getRight();
+
+                    long startSingleTime = System.currentTimeMillis();
+                    SingleDerivationAnswerTreeBuilder.getInstance().buildParseTree(query.originalParseTree, wordMappings).getParseTree();
+                    long endSingleTime = System.currentTimeMillis();
+                    long startMultipleTime = System.currentTimeMillis();
+                    new MultipleDerivationFactorizedAnswerTreeBuilder(new QueryBasedFactorizer(query.originalParseTree)).buildParseTree(query.originalParseTree, wordMappings).getParseTree();
+                    long endMultipleTime = System.currentTimeMillis();
+                    long startSummarizedTime = System.currentTimeMillis();
+                    MultipleDerivationSummarizedAnswerTreeBuilder.getInstance().buildParseTree(query.originalParseTree, wordMappings).getParseTree();
+                    long endSummarizedTime = System.currentTimeMillis();
+
+                    Expression factorize = new QueryBasedFactorizer(query.originalParseTree).factorize(wordMappings);
+
+
+                    System.out.println(String.format("%20d\t%20d\t%20d\t%20d\t%20d\t%20d\t%20d", uniqueValues, wordMappings.getLastDerivation() + 1, wordMappings.getWordMappingByDerivation().get(0).size(), endSingleTime - startSingleTime, endMultipleTime - startMultipleTime, endSummarizedTime - startSummarizedTime, getExpressionSize(factorize)));
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    private static Map<ITuple, Pair<WordMappings, Integer>> getResultsAndWordMappings(String queryName) throws Exception {
+        Map<ITuple, Pair<WordMappings, Integer>> result = new TreeMap<>();
+
+        if (queryName.equals("query00_init")) {
+            WordMappings wordMappings = new WordMappings();
+            wordMappings.add(0, 3, "ans");
+            ITuple tuple = new Tuple(Arrays.<ITerm>asList(new StringTerm("ans")));
+            result.put(tuple, new ImmutablePair<WordMappings, Integer>(wordMappings, 1));
+        } else {
+            for (int i = 0; i < 50; i++) {
+                WordMappings wordMappings = new WordMappings();
+                ITuple tuple = new Tuple(Arrays.<ITerm>asList(new StringTerm("ans" + i)));
+                result.put(tuple, new ImmutablePair<WordMappings, Integer>(wordMappings, i * 100));
+                for (int j = 0; j < 5000; j++) {
+                    int author = (int) (Math.random() * i * 100);
+                    int paper = (int) (Math.random() * i * 100);
+                    int year = (int) (Math.random() * i * 100);
+                    int conference = (int) (Math.random() * i * 100);
+                    switch (queryName) {
+                        case "query03":
+                            wordMappings.add(j, 3, "ans" + i);
+
+                            wordMappings.add(j, 6, "paper" + paper);
+                            wordMappings.add(j, 10, "year" + year);
+                            break;
+                        case "query08":
+                            wordMappings.add(j, 3, "ans" + i);
+                            wordMappings.add(j, 5, "conference" + conference);
+                            break;
+                        case "query09":
+                            wordMappings.add(j, 3, "ans" + i);
+                            wordMappings.add(j, 6, "paper" + paper);
+                            wordMappings.add(j, 9, "conference" + conference);
+                            break;
+                        case "query11":
+                            wordMappings.add(j, 3, "ans" + i);
+                            wordMappings.add(j, 5, "author" + author);
+                            wordMappings.add(j, 8, "paper" + paper);
+                            wordMappings.add(j, 11, "conference" + conference);
+                            wordMappings.add(j, 13, "year" + year);
+                            break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+
+    private static int getExpressionSize(Expression expression) {
+        int size = expression.getVariables().size();
+        for (Expression subExpression : expression.getExpressions()) {
+            size += getExpressionSize(subExpression);
+        }
+        return size;
+
+    }
+}
